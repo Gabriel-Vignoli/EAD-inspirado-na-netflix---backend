bd3a06493f60e4a010aca5914868ffca117c4a96
Gabriel Vignoli <vignoligabriel052@gmail.com>
2024-04-20 19:58:54 -0300

    Relacionamentos entre modelos

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,14 @@
 
 import express from 'express'
 import { sequelize } from './database'
+import { adminJs, adminJsRouter } from './adminjs'
 
 const app = express()
+
+app.use(express.static("public"))
+
+// app.use(caminho, rotas)
+app.use(adminJs.options.rootPath, adminJsRouter)
 
 const PORT = process.env.PORT || 3000
 
@@ -11,6 +17,6 @@ app.listen(PORT, () => {
   sequelize.authenticate().then(() => {
     console.log('DB connection successfull')
   })
-  
+
   console.log(`Server started successfuly at port ${PORT}`)
 })
