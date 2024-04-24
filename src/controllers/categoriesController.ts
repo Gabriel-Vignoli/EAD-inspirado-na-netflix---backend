61f57535a3cf1fd114a4df959428ae89330276e6
Gabriel Vignoli <vignoligabriel052@gmail.com>
2024-04-24 20:14:29 -0300

    Obtendo cursos de uma categoria

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express'
 import { getPaginationParams } from '../helpers/getPaginationParams'
-import { categoryService } from '../services/categoriesController'
+import { categoryService } from '../services/categoryService'
 
 const categoriesController = {
+  // GET/categories
   index: async (req: Request, res: Response) => {
     const [page, perPage] = getPaginationParams(req.query)
 
@@ -15,7 +16,21 @@ const categoriesController = {
         return res.status(400).json({ message: err.message })
       }
     }
+  },
+
+  	// GET /categories/:id
+    show: async (req: Request, res: Response) => {
+      const { id } = req.params
+  
+      try {
+        const category = await categoryService.findByIdWithCourses(id)
+        return res.json(category)
+      } catch (err) {
+        if (err instanceof Error) {
+          return res.status(400).json({ message: err.message })
+        }
+      }
+    }
   }
-}
 
 export { categoriesController }
