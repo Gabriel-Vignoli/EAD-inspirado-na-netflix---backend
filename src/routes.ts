210bd7a3a537f3a7fa5547c002af8f5e0e8094a9
Gabriel Vignoli <vignoligabriel052@gmail.com>
2024-04-28 14:27:08 -0300

    Obtendo os 10 cursos mais populares

--- src/routes.ts
+++ src/routes.ts
@@ -12,19 +12,24 @@ const router = express.Router()
 router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
 
+
 router.get('/categories', ensureAuth, categoriesController.index)
 router.get('/categories/:id', ensureAuth, categoriesController.show)
 
+
 router.get('/courses/featured', ensureAuth, coursesController.featured)
-router.get('/courses/newest', coursesController.newest)
+router.get('/courses/popular', ensureAuth, coursesController.popular)
 router.get('/courses/search', ensureAuth, coursesController.search)
 router.get('/courses/:id', ensureAuth, coursesController.show)
 
+
 router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream)
+
 
 router.get('/favorites', ensureAuth, favoritesController.index)
 router.post('/favorites', ensureAuth, favoritesController.save)
 router.delete('/favorites/:id', ensureAuth, favoritesController.delete)
+
 
 router.post('/likes', ensureAuth, likesController.save)
 router.delete('/likes/:id', ensureAuth, likesController.delete)
